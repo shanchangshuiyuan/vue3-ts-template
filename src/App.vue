5afb8c40e24d184706d3f25d77272b7e9ae29d5a
zgy <2937958677@qq.com>
2024-06-20 20:23:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const a = 100;
+console.log(a);
+
+</script>
+
+<style scoped>
+.box {
+  width: 600px;
+  height: auto;
+  background-color: red
+}
+</style>
