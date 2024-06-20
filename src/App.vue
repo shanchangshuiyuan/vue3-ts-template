a1059c956fc31d11d1694e64727c7bcedab3ff16
zgy <2937958677@qq.com>
2024-06-20 20:31:32 +0800

    格式化代吗

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,12 @@
 <script setup lang="ts">
 const a = 100;
 console.log(a);
-
 </script>
 
 <style scoped>
 .box {
   width: 600px;
   height: auto;
-  background-color: red
+  background-color: red;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import App from "./App.vue";
 
 createApp(App).mount("#app");
 
-for(let i=0;i<10;i++){
-    console.log(i);
+for (let i = 0; i < 10; i++) {
+  console.log(i);
 }
